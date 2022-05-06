b31558c364c0642c1a5cb481077509a8d24c6ce9
Daniel <vansdan@web.de>
2022-05-06 22:07:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
-setup(name='cbpi4-ilc-sensor',
+setup(name='cbpi4-ilc-sensordata',
       version='0.0.1',
       description='Generic CraftBeerPi ILC Sensor Plugin',
       author='Daniel Lauterbach',
       author_email='vansdan@web.de'
-      url='https://github.com/vansdan/cbpi4-ilc-sensor',
+      url='https://github.com/vansdan/cbpi4-ilc-sensordata',
       license='GPLv3',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
       '': ['*.txt', '*.rst', '*.yaml'],
       'cbpi4-http-actor': ['*','*.txt', '*.rst', '*.yaml']},
-      packages=['cbpi4-ilc-sensor'],
+      packages=['cbpi4-ilc-sensordata'],
       install_requires=[
             'cbpi>=4.0.0.34',
       ],
